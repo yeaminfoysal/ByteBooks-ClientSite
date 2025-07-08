9068d53ee03670e95572b411a39315eb68b1a5bc
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-07-09 00:24:26 +0600

    initialized redux toolkit & rtk query

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,15 @@ import {
 } from "react-router";
 import router from './routes/index.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx';
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>,
 )
