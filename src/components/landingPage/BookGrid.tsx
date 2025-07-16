6528e0cb00636f6854d518371bbbd7bf8eaef448
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-07-16 20:06:53 +0600

    update books laypout

--- src/components/landingPage/BookGrid.tsx
+++ src/components/landingPage/BookGrid.tsx
@@ -4,15 +4,13 @@ import BookCard from './BookCard';
 
 interface BookGridProps {
   books: Book[];
-  // onUpdateBook: (book: Book) => void;
-  // onDeleteBook: (bookId: string) => void;
 }
 
 const BookGrid: React.FC<BookGridProps> = ({ books }) => {
   return (
     <div className="w-full max-w-7xl mx-auto px-4 py-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {books.map((book) => (
+        {books.slice(0, 8).map((book) => (
           <BookCard
             key={book._id}
             book={book}
